49eba8ab8ebe19bcd4f6f2fc1f4b2852eca1e0c8
Duncan Fermor <duncan@elitemail.org>
2015-07-07 16:54:53 +0100

    added d3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 run this with a simple webserver, e.g:
 python -m SimpleHTTPServer
+
+to get d3 working with reactjs (possibly a simplehttp server bug) add utf-8 to each of the script tags
--- js/index.jsx
+++ js/index.jsx
@@ -1,9 +1,14 @@
 var LineGraph = React.createClass({
 	render: function() {
+		var graph = <svg className="lineGraph"></svg>
+		
+		var xRange = d3.scale.linear()
+		  .range([40, 400]) // the amount of the svg to cover
+			.domain([0, 100]) // the range of values we'll be showing
+		var yRange = d3.scale.linear().range([40, 400]).domain([0, 100])
+
 		return (
-			<div className="lineGraph">
-				So many lines...
-			</div>
+			graph
 		);
 	}
 });
