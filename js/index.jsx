52c6cc0b9a0b538258098d670cb034d303b37da9
Duncan Fermor <duncan@elitemail.org>
2015-07-08 14:17:03 +0100

    d3 and react rendering one after the other

--- js/index.jsx
+++ js/index.jsx
@@ -1,12 +1,20 @@
 var LineGraph = React.createClass({
-	render: function() {
-		var graph = <svg className="lineGraph"></svg>
-		
+	componentDidMount: function() {
 		var xRange = d3.scale.linear()
 		  .range([40, 400]) // the amount of the svg to cover
-			.domain([0, 100]) // the range of values we'll be showing
-		var yRange = d3.scale.linear().range([40, 400]).domain([0, 100])
+			.domain([0, 100]); // the range of values we'll be showing
+		var yRange = d3.scale.linear().range([40, 400]).domain([0, 100]);
 
+		var xAxis = d3.svg.axis().scale(xRange);
+		var yAxis = d3.svg.axis().scale(yRange).orient("left");
+
+		var graph = React.findDOMNode(this);
+		d3_graph = d3.select(graph);
+		d3_graph.append("svg:g").call(xAxis);
+		d3_graph.append("svg:g").call(yAxis).attr("transform", "translate(40, 0)");
+	},
+	render: function() {
+		var graph = <svg className="lineGraph"></svg>
 		return (
 			graph
 		);
